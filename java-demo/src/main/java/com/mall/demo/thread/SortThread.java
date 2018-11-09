43ad12adfbdc8a9e2dc2df94ad49466135e405d7
fengchao <fengchaodexiatian@126.com>
2018-11-09 22:49:42 +0800

    小例子编写

---
+++ java-demo/src/main/java/com/mall/demo/thread/SortThread.java
@@ -0,0 +1,34 @@
+package com.mall.demo.thread;
+
+import org.apache.tools.ant.types.resources.Sort;
+
+/**
+ * @author pc-fengc
+ * created by pc-fengc on 2018-11-09 16:08.
+ */
+public class SortThread implements Runnable{
+
+    private int num;
+
+    public SortThread(int num) {
+        this.num = num;
+    }
+
+
+    public static void main(String[] args) {
+        int[] nums = {2,6,4,11,3,998,5455,1,5,152,15,990};
+        for (int i = 0 ; i < nums.length ;i++) {
+            new Thread(new SortThread(nums[i])).start();
+        }
+    }
+
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(num);
+            System.out.println(num);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
