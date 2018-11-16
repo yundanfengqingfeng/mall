510a718619ec167c0a38773a765ec4dcfc5a8fd1
fengchao <fengchaodexiatian@126.com>
2018-11-16 16:32:13 +0800

    提交后台vue页面代码

---
+++ mall-admin/mall-admin-soringboot/src/main/java/com/mall/admin/springboot/malladminsoringboot/MallAdminSoringbootApplication.java
@@ -0,0 +1,12 @@
+package com.mall.admin.springboot.malladminsoringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallAdminSoringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallAdminSoringbootApplication.class, args);
+    }
+}
